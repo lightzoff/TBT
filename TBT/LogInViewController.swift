8d67a308610a7db2e13eebf76ff6fd5362ba5c14
Dawid <dawid@dawids-macbook-pro.local>
2015-09-25 17:40:35 +1200

    Loginview

---
+++ TBT/LogInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LogInViewController.swift
+//  TBT
+//
+//  Created by Dawid on 25/09/15.
+//  Copyright © 2015 Dawid. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
